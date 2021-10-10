2e77848ee1d74afa4c978fa893b9b6e300834f47
namanDaBoss <67775905+namanDaBoss@users.noreply.github.com>
2021-10-10 16:58:04 +0530

    final polishing

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 import time
+from helpers import apology
 
 # Configure application
 app = Flask(__name__)
@@ -15,6 +16,8 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
+
+
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -22,8 +25,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 """ app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -56,35 +57,86 @@ def index():
 
     return render_template("index.html", products=outList)
 
-@app.route("/edit",methods=["GET", "POST"])
-def edit():
+# @app.route("/post/<string:post_slug>", methods=['GET'])
+# def post_route(post_slug):
+#     post = Posts.query.filter_by(slug=post_slug).first()
+#     return render_template('post.html', params=params, post=post
+
+
+@app.route("/edit/<id_slug>", methods=["GET", "POST"])
+def edit(id_slug):
     """Editing product info"""
     if request.method == 'GET':
-        return render_template("edit.html")
-    else:
-        units = int(request.form.get("units"))
-        prodid = int(request.form.get("prodid"))
-        db.execute("UPDATE products SET units = :units WHERE product_id = :prodid", units=units, prodid=prodid)
-        return render_template('edit.html')
+        row = db.execute(
+            "SELECT * FROM products WHERE productid = :id", id=id_slug)
+        print(row)
 
+        outList = []
+        outList.append(list((row[0]).values()))
+        return render_template('edit.html', product=outList[0])
+        # return render_template("edit.html")
+    else:
+        row = db.execute(
+            "SELECT * FROM products WHERE productid = :id", id=id_slug)
+        try:
+            units = int(request.form.get("units"))
+        except:
+            units = row.get('units')
+        try:
+            location = int(request.form.get("location"))
+        except:
+            location = row.get('location')
+        try:
+            p_name = int(request.form.get("p_name"))
+        except:
+            p_name = row.get('p_name')
+        try:
+            seller_name = int(request.form.get("seller_name"))
+        except:
+            seller_name = row.get('seller_name')
+        try:
+            seller_phone = int(request.form.get("seller_phone"))
+        except:
+            seller_phone = row.get('seller_phone')
+        try:
+            seller_email = int(request.form.get("seller_email"))
+        except:
+            seller_email = row.get('seller_email')
+
+        ntime = time.time()
+
+        db.execute("UPDATE products SET p_name=p_name, seller_name=seller_name , units=units,seller_phone=seller_phone,seller_email=seller_email, location=location, time=time) WHERE productid = :prodid",
+                   p_name=p_name, seller_name=seller_name, units=units, seller_email=seller_email, location=location, seller_phone=seller_phone, time=ntime, prodid=id_slug)
+        # db.execute("UPDATE products SET units = :units WHERE productid = :prodid", units=units, prodid=prodid)
+        return render_template('index.html')
+
+
+def maps():
+    """Opens location in google maps"""
+    
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
     """Adding new product"""
     if request.method == 'GET':
         return render_template("add.html")
     else:
-        time = time.time()
+        ntime = time.time()
         p_name = str(request.form.get("p_name"))
         seller_name = str(request.form.get("seller_name"))
+        print(request.form.get('units'))
         units = int(request.form.get("units"))
-        seller_phone = int(request.form.get("seller_phone"))
+        seller_phone = (request.form.get("seller_phone"))
         seller_email = str(request.form.get("seller_email"))
         location = str(request.form.get("location"))
-        product_desc = str(request.form.get("product_desc"))
-        
-        # add time remove product desc in db
-        db.execute("INSERT INTO products(p_name,seller_name,units,seller_phone,seller_email,location, product_desc, time) VALUES (:p_name, :seller_name, :units,:seller_phone, :seller_email,:location, :product_desc, :time)", p_name=p_name, seller_name= seller_name, units=units,seller_email=seller_email, location=location, product_desc=product_desc, time=time)
+        # product_desc = str(request.form.get("product_desc"))
 
-        return render_template("add.html")
+        # add time remove product desc in db
+        db.execute("INSERT INTO products(p_name,seller_name,units,seller_phone,seller_email, location, time) VALUES (:p_name, :seller_name, :units,:seller_phone, :seller_email,:location, :time)",
+                   p_name=p_name, seller_name=seller_name, units=units, seller_email=seller_email, location=location, seller_phone=seller_phone, time=ntime)
+
+        return render_template("index.html")
+
 
 def errorhandler(e):
     """Handle error"""
